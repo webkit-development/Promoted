1792a0113b11323a6bbd788732c6a992dd96a514
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-20 09:24:37 -0400

    Initial Commit

---
+++ Promoted/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Promoted
+//
+//  Created by Kevin Stradtman on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Promoted/PromotedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromotedApp.swift
+//  Promoted
+//
+//  Created by Kevin Stradtman on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct PromotedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
